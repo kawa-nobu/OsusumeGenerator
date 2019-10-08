bf2019ae63446087a040c1bd90a24d8a3c77d704
kawa-nobu <44832116+kawa-nobu@users.noreply.github.com>
2019-10-09 00:35:08 +0900

    初投稿
    色々とおかしい点が...

---
+++ main.js
@@ -0,0 +1,94 @@
+    //位置標準
+    var rl_num = 275;
+    var upd_num = 125;
+    var bs_num = 100;
+    var font_1_num = 24;
+    var font_2_num = 42;
+    //コントロール用関数
+    function right() {
+      document.getElementById('r').onclick = function () {
+        rl_num = rl_num + 5;
+        img_add();
+      }
+    }
+
+    function left() {
+      document.getElementById('l').onclick = function () {
+        rl_num = rl_num - 5;
+        img_add();
+      }
+    }
+
+    function up() {
+      document.getElementById('up').onclick = function () {
+        upd_num = upd_num - 5;
+        img_add();
+      }
+    }
+
+    function down() {
+      document.getElementById('down').onclick = function () {
+        upd_num = upd_num + 5;
+        img_add();
+      }
+    }
+
+    function big() {
+      document.getElementById('big').onclick = function () {
+        bs_num = bs_num + 5;
+        img_add();
+      }
+    }
+
+    function small() {
+      document.getElementById('small').onclick = function () {
+        bs_num = bs_num - 5;
+        img_add();
+      }
+    }
+    //フォント処理関数
+    function add_text1() {
+      var text1 = document.getElementById('text1').value;
+      var out = document.getElementById('output');
+      var canv = out.getContext('2d');
+      canv.font = "bold " + font_1_num + "px Noto Sans JP";
+      canv.fillText(text1, 404, 229);
+    }
+
+    function add_text2() {
+      var text2 = document.getElementById('text2').value;
+      var out = document.getElementById('output');
+      var canv = out.getContext('2d');
+      canv.font = "bold " + font_2_num + "px Noto Sans JP";
+      canv.fillText(text2, 380, 268);
+    }
+    //画像処理関数
+    function img_add() {
+      if (document.getElementById('add').files[0] != undefined) {
+        var out = document.getElementById('output');
+        var canv = out.getContext('2d');
+        var img = new Image();
+        img.src = window.URL.createObjectURL(document.getElementById('add').files[0]);
+        img.onload = function () {
+          canv.clearRect(0, 0, 760, 430);
+          canv.drawImage(img, rl_num, upd_num, bs_num, bs_num);
+          run();
+        }
+      } else {
+        window.alert("画像が指定されていません！");
+      }
+    }
+
+    function run() {
+      var out = document.getElementById('output');
+      var canv = out.getContext('2d');
+      var img = new Image();
+      img.src = "./OSUSUME.png";
+      img.onload = function () {
+        canv.drawImage(img, 0, 0, 760, 430);
+        font_1_num = document.getElementById('font1').value;
+        add_text1();
+        font_2_num = document.getElementById('font2').value;
+        add_text2();
+      }
+    }
